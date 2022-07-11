07d46ba521bb4eb799b98bf57a70a38cced29582
Damilolai <ipinmisho.damilola@gmail.com>
2022-07-11 09:45:02 +0100

    js updated

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
- function myFunction (){
+let acc = document.getElementById('account-name').value;
+let accname = document.getElementById('instincthub');
+let box = document.getElementById('box');
+let subbmitt = document.getElementById('mySubmit');
 
-    document.getElementById('mySubmit').submit(yes)
+function dami (){
+    if(acc='Bola'){
+        box.style.display='block'
+    }
 }
+subbmitt.addEventListener('submit',dami)
+
+
+
+
+
+
+
+
+let Details = [
+    {
+      CUSTOMER: "Instinct Hub LTD",
+      ACCOUNT,OFFICER: "KEHINDE AKINTOYE",
+      ACCOUNT,OPENING,DATE: "30th January 2022",
+    },
+  ];
